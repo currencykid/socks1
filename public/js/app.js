c2e45f0fa87f8f5c047efbf8c17d1d28de575e8d
ck <thecurrencykid@gmail.com>
2016-06-12 16:28:25 -0700

    messages UI

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,26 @@ socket.on('connect', function(){
 socket.on('message', function(message){
 	console.log("New message");
 	console.log(message.text);
+
+	jQuery('.messages').append('<p>' + message.text +'</p>'); 
+});
+
+//handles new message submit
+
+var $form = jQuery('#message-form'); 
+
+$form.on('submit', function(event){
+		//when u don't wanna submit by refreshing the page
+		// using sockets so we can handle form submission on our own
+	event.preventDefault();	
+
+	var $message = $form.find('input[name=message]') ;
+
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('');
+
+
 });
